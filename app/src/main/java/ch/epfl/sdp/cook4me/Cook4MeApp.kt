45f652800fc804a504f3483f0954140b11f44103
bu-da <daniel.bucher@healthinal.com>
2023-03-02 15:39:03 +0100

    use patched version of jacoco (provided by pawnies) to be compliant with jetpack compose

---
+++ app/src/main/java/ch/epfl/sdp/cook4me/Cook4MeApp.kt
@@ -0,0 +1,39 @@
+package ch.epfl.sdp.cook4me
+
+import androidx.annotation.StringRes
+import androidx.compose.runtime.*
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import ch.epfl.sdp.cook4me.ui.ProfileScreen
+import ch.epfl.sdp.cook4me.ui.WelcomeScreen
+
+/**
+ * enum values that represent the screens in the app
+ */
+enum class Screen {
+    Start,
+    Profile
+}
+
+@Composable
+fun Cook4MeApp(
+    navController: NavHostController = rememberNavController()
+) {
+    var name by remember {
+        mutableStateOf("")
+    }
+
+    NavHost(navController = navController, startDestination = Screen.Start.name) {
+        composable(route = Screen.Start.name) {
+            WelcomeScreen(onStartButtonClicked = {
+                name = it
+                navController.navigate(Screen.Profile.name)
+            })
+        }
+        composable(route = Screen.Profile.name) {
+            ProfileScreen(name)
+        }
+    }
+}
