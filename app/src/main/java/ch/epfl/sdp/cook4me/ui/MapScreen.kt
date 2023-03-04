3549af6b9c5c085d4ec8fa4e3daf98c23a12c9e0
pauromeu <romeu.pau@gmail.com>
2023-03-04 09:55:28 +0100

    fix magic numbers in MapScreen

--- app/src/main/java/ch/epfl/sdp/cook4me/ui/MapScreen.kt
+++ app/src/main/java/ch/epfl/sdp/cook4me/ui/MapScreen.kt
@@ -3,6 +3,8 @@ package ch.epfl.sdp.cook4me.ui
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import ch.epfl.sdp.cook4me.R
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
@@ -12,14 +14,12 @@ import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
 fun MapScreen() {
-    val satLat = 46.520544
-    val satLng = 6.567825
-    val zoomSat = 10f
+    val SATELLITE_LAT = 46.520544
+    val SATELLITE_LON = 6.567825
 
-
-    val satellite = LatLng(satLat, satLng)
+    val satellite = LatLng(SATELLITE_LAT, SATELLITE_LON)
     val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(satellite, zoomSat)
+        position = CameraPosition.fromLatLngZoom(satellite, 10f)
     }
     GoogleMap(
         modifier = Modifier.fillMaxSize(),
