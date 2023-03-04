997c9c1e83a5df035006762c440e72a55e299825
pauromeu <romeu.pau@gmail.com>
2023-03-04 09:14:22 +0100

    add local properties file to pass CI

--- .cirrus/populate_properties.sh
+++ .cirrus/populate_properties.sh
@@ -3,3 +3,4 @@
 # Takes $GITHUB_USERNAME and $GITHUB_TOKEN and writes them to the ~/.gradle/gradle.properties
 mkdir ~/.gradle
 echo "githubJacocoUsername=$GITHUB_USERNAME\ngithubJacocoPassword=$GITHUB_TOKEN" > ~/.gradle/gradle.properties
+echo MAPS_API_KEY=$MAPS_API_KEY >> local.properties
