10969b72f3732c0dee79a52b4eb1bb3fe17897f7
bu-da <daniel.bucher@healthinal.com>
2023-03-06 15:15:29 +0100

    update common functions to have expected return types

--- app/src/main/java/ch/epfl/sdp/cook4me/persistence/repository/common.kt
+++ app/src/main/java/ch/epfl/sdp/cook4me/persistence/repository/common.kt
@@ -3,8 +3,9 @@ package ch.epfl.sdp.cook4me.persistence.repository
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
 
-suspend fun <T : Any> FirebaseFirestore.add(collectionPath: String, value: T) =
+suspend fun <T : Any> FirebaseFirestore.add(collectionPath: String, value: T) {
     collection(collectionPath).add(value).await()
+}
 
 suspend inline fun <reified T : Any> FirebaseFirestore.getAll(collectionPath: String): Map<String, T> {
     val result = collection(collectionPath).get().await()
@@ -18,5 +19,6 @@ suspend inline fun <reified T : Any> FirebaseFirestore.getById(
     collection(collectionPath).document(id).get().await()
         .toObject(T::class.java)
 
-suspend fun <T : Any> FirebaseFirestore.update(collectionPath: String, id: String, value: T) =
+suspend fun <T : Any> FirebaseFirestore.update(collectionPath: String, id: String, value: T) {
     collection(collectionPath).document(id).set(value).await()
+}
