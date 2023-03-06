c69c030d9ff4f95eaf35c11b826b2d6e1edbdad5
bu-da <daniel.bucher@healthinal.com>
2023-03-06 15:43:20 +0100

    reformat code

--- app/src/main/java/ch/epfl/sdp/cook4me/persistence/repository/RecipeRepository.kt
+++ app/src/main/java/ch/epfl/sdp/cook4me/persistence/repository/RecipeRepository.kt
@@ -9,13 +9,12 @@ const val COLLECTION_PATH = "recipes"
 
 class RecipeRepository(
     private val store: FirebaseFirestore = Firebase.firestore,
-)
- {
+) {
     suspend fun add(value: Recipe) = store.add(COLLECTION_PATH, value)
 
-     suspend fun getAll() = store.getAll<Recipe>(COLLECTION_PATH)
+    suspend fun getAll() = store.getAll<Recipe>(COLLECTION_PATH)
 
-     suspend fun getById(id: String) = store.getById<Recipe>(COLLECTION_PATH, id)
+    suspend fun getById(id: String) = store.getById<Recipe>(COLLECTION_PATH, id)
 
-     suspend fun update(id: String, value: Recipe) = store.update(COLLECTION_PATH, id, value)
+    suspend fun update(id: String, value: Recipe) = store.update(COLLECTION_PATH, id, value)
 }
