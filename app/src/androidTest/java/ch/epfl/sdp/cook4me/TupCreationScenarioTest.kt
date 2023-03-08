5b3e6307258e3e8720a9c7b37113f351f2b9f6a1
John Taylor <john.taylor@epfl.ch>
2023-03-08 14:35:09 +0100

    create tupperform test added

---
+++ app/src/androidTest/java/ch/epfl/sdp/cook4me/TupCreationScenarioTest.kt
@@ -0,0 +1,57 @@
+package ch.epfl.sdp.cook4me
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.LocalActivityResultRegistryOwner
+import androidx.activity.result.ActivityResultRegistry
+import androidx.activity.result.ActivityResultRegistryOwner
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.core.app.ActivityOptionsCompat
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import ch.epfl.sdp.cook4me.ui.TupCreationScreen
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class TupCreationScenarioTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun createNewTupperware() {
+        val testUri = Uri.parse("testUri")
+
+        composeTestRule.setContent {
+
+
+            val registryOwner = ActivityResultRegistryOwner {
+                object : ActivityResultRegistry() {
+                    override fun <I : Any?, O : Any?> onLaunch(
+                        requestCode: Int,
+                        contract: ActivityResultContract<I, O>,
+                        input: I,
+                        options: ActivityOptionsCompat?
+                    ) {
+                        // don't launch an activity, just respond with the test Uri
+                        val intent = Intent().setData(testUri)
+                        this.dispatchResult(requestCode, Activity.RESULT_OK, intent)
+                    }
+                }
+            }
+            CompositionLocalProvider(LocalActivityResultRegistryOwner provides registryOwner) {
+                // any composable inside this block will now use our mock ActivityResultRegistry
+                TupCreationScreen(
+                    onClickTakePhoto = {},
+                    onClickAddImage = {},
+                )
+            }
+        }
+
+    }
+}
