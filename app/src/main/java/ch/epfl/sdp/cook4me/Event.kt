3703e9c0f7089854e7d6e9ca41bf3ec846303f1e
dayan9265 <82887324+dayan9265@users.noreply.github.com>
2023-03-09 12:15:24 +0100

    Use of calendar instead of string for the date

--- app/src/main/java/ch/epfl/sdp/cook4me/Event.kt
+++ app/src/main/java/ch/epfl/sdp/cook4me/Event.kt
@@ -1,10 +1,11 @@
 package ch.epfl.sdp.cook4me
 
+import java.util.*
+
 class Event {
     var name: String = ""
     var description: String = ""
-    var date: String = ""
-    var time: String = ""
+    var dateTime: Calendar = Calendar.getInstance()
     var location: String = ""
     var maxParticipants: Int = 0
     var participants: List<String> = listOf()
@@ -12,10 +13,8 @@ class Event {
     var id: String = ""
     var isPrivate: Boolean = false
 
-    constructor()
-
     fun isValidEvent(): Boolean {
-        return name.isNotEmpty() && description.isNotEmpty() && date.isNotEmpty() && time.isNotEmpty() && location.isNotEmpty() && maxParticipants > 1
+        return name.isNotEmpty() && description.isNotEmpty() && location.isNotEmpty() && maxParticipants > 1
     }
 
     /**
@@ -25,8 +24,6 @@ class Event {
     fun eventProblem(): String{
         if (name.isEmpty()) return "Name is empty"
         if (description.isEmpty()) return "Description is empty"
-        if (date.isEmpty()) return "Date is empty"
-        if (time.isEmpty()) return "Time is empty"
         if (location.isEmpty()) return "Location is empty"
         if (maxParticipants < 2) return "Max participants is less than 2"
         return ""
--- app/src/main/java/ch/epfl/sdp/cook4me/ui/CreateEvent.kt
+++ app/src/main/java/ch/epfl/sdp/cook4me/ui/CreateEvent.kt
@@ -8,21 +8,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import ch.epfl.sdp.cook4me.ui.simpleComponent.AddressInputReader
-import ch.epfl.sdp.cook4me.ui.simpleComponent.InputTextReader
-import ch.epfl.sdp.cook4me.ui.simpleComponent.IntegerSlider
-import ch.epfl.sdp.cook4me.ui.simpleComponent.ToggleButtonChoice
+import ch.epfl.sdp.cook4me.Event
+import ch.epfl.sdp.cook4me.ui.simpleComponent.*
+import java.util.*
 
 @Composable
 fun CreateEvent() {
+    val event = Event()
+
     Column (
         verticalArrangement = androidx.compose.foundation.layout.Arrangement.spacedBy(8.dp)
     ) {
-        InputTextReader(question = "What is the location?", onTextChanged = {})
+        InputTextReader(question = "Name of the event?", onTextChanged = {event.name = it})
         AddressInputReader()
-        IntegerSlider(text = "Number of people to invite", min = 2, max = 16, onValueChange = {}, modifier = Modifier.fillMaxWidth())
-        ToggleButtonChoice(question = "Who can see the event", possibilities = Pair("Everyone","Subscriber only"), onToggle = {})
+        IntegerSlider(text = "Number of people to invite", min = 2, max = 16, onValueChange = {event.maxParticipants=it}, modifier = Modifier.fillMaxWidth())
+        ToggleButtonChoice(question = "Who can see the event", possibilities = Pair("Everyone","Subscriber only"), onToggle = {event.isPrivate = it == "Subscriber only"})
         // submit button
+        DatePickerComponent(initialDate = Calendar.getInstance(), onDateSelected = {event.dateTime.set(it.get(Calendar.YEAR), it.get(Calendar.MONTH), it.get(Calendar.DAY_OF_MONTH))})
         Button(onClick = { /*TODO*/ }, modifier = Modifier.align(Alignment.End)) {
             Text(text = "Submit")
         }
