0e52ab2ff296183a5a107ed7b3b030a31676ee92
Tachi-67 <haolongli_neko@outlook.com>
2023-03-29 10:11:28 +0200

    add password valid check

--- app/src/main/java/ch/epfl/sdp/cook4me/application/AccountService.kt
+++ app/src/main/java/ch/epfl/sdp/cook4me/application/AccountService.kt
@@ -13,7 +13,11 @@ class AccountService(private val auth: FirebaseAuth = FirebaseAuth.getInstance()
     fun isValidEmail(email: String): Boolean =
         email.isNotBlank() && Patterns.EMAIL_ADDRESS.matcher(email).matches()
 
+    fun isValidPassword(password: String): Boolean =
+        password.isNotBlank() && password.length >= 6
+
     suspend fun register(email: String, password: String) {
         auth.createUserWithEmailAndPassword(email, password).await()
     }
+
 }
