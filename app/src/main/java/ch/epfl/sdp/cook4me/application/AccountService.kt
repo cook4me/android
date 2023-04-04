4a83dcdbb9c3b03ea65a6a3cd985009fd99b1608
Tachi-67 <haolongli_neko@outlook.com>
2023-04-04 19:26:46 +0200

    display user email & ui test

--- app/src/main/java/ch/epfl/sdp/cook4me/application/AccountService.kt
+++ app/src/main/java/ch/epfl/sdp/cook4me/application/AccountService.kt
@@ -3,11 +3,20 @@ package ch.epfl.sdp.cook4me.application
 import android.util.Patterns
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.tasks.await
 
 class AccountService(private val auth: FirebaseAuth = FirebaseAuth.getInstance()) {
     private val _minPasswordLength = 6
+    private val _userEmail = MutableStateFlow<String?>(null)
+    var userEmail = _userEmail.asStateFlow()
 
+    init {
+        auth.addAuthStateListener {
+            _userEmail.value = it.currentUser?.email
+        }
+    }
     fun getCurrentUser(): FirebaseUser? =
         auth.currentUser
 
--- app/src/main/java/ch/epfl/sdp/cook4me/ui/overview/OverviewViewModel.kt
+++ app/src/main/java/ch/epfl/sdp/cook4me/ui/overview/OverviewViewModel.kt
@@ -2,7 +2,11 @@ package ch.epfl.sdp.cook4me.ui.overview
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import ch.epfl.sdp.cook4me.application.AccountService
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class OverviewViewModel(
     private val accountService: AccountService = AccountService()
@@ -12,7 +16,15 @@ class OverviewViewModel(
     private val _navigationState = mutableStateOf<Int>(0)
     val navigationState
         get() = _navigationState
+    private val _userEmail = MutableStateFlow<String?>(null)
+    val userEmail: StateFlow<String?>
+        get() = _userEmail
 
+    init {
+        viewModelScope.launch {
+            accountService.userEmail.collect { _userEmail.value = it }
+        }
+    }
     fun onSignOutButtonClicked() {
         accountService.signOut()
         _navigationState.value = 1
