fa35f7dc301b296c5b3e7c2dabb7f3952c061f99
dayan9265 <82887324+dayan9265@users.noreply.github.com>
2023-04-10 12:28:32 +0200

    add RecipeListScreen test

---
+++ app/src/androidTest/java/ch/epfl/sdp/cook4me/ui/recipeFeed/RecipeListScreenTest.kt
@@ -0,0 +1,37 @@
+package ch.epfl.sdp.cook4me.ui.recipeFeed
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import ch.epfl.sdp.cook4me.persistence.model.Recipe
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class RecipeListScreenTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun defaultRecipeListScreenIsDisplayed() {
+        val mockList = listOf(
+            Pair(Recipe(name = "Test recipe 1", cookingTime = "10 min"), 1),
+            Pair(Recipe(name = "Test recipe 2", cookingTime = "20 min"), 2)
+        )
+        composeTestRule.setContent {
+            RecipeListScreen(recipeList = mockList)
+        }
+
+        composeTestRule.onNodeWithText("Test recipe 1").assertIsDisplayed()
+        composeTestRule.onNodeWithText("10 min").assertIsDisplayed()
+        composeTestRule.onNodeWithText("1").assertIsDisplayed()
+
+        composeTestRule.onNodeWithText("Test recipe 2").assertIsDisplayed()
+        composeTestRule.onNodeWithText("20 min").assertIsDisplayed()
+        composeTestRule.onNodeWithText("2").assertIsDisplayed()
+    }
+}
