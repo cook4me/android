437c6932b21fc7e6b5dcfed6d5d489c95986d2e2
pauromeu <romeu.pau@gmail.com>
2023-04-18 19:39:02 +0200

    add bottom bar navigation and adapt app entry point

--- app/src/main/java/ch/epfl/sdp/cook4me/Cook4MeApp.kt
+++ app/src/main/java/ch/epfl/sdp/cook4me/Cook4MeApp.kt
@@ -1,14 +1,43 @@
 package ch.epfl.sdp.cook4me
 
 import SignUpScreen
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.createGraph
 import ch.epfl.sdp.cook4me.persistence.model.Post
 import ch.epfl.sdp.cook4me.ui.OverviewScreen
 import ch.epfl.sdp.cook4me.ui.eventform.CreateEventScreen
@@ -17,6 +46,7 @@ import ch.epfl.sdp.cook4me.ui.eventform.Event
 import ch.epfl.sdp.cook4me.ui.login.LoginScreen
 import ch.epfl.sdp.cook4me.ui.map.GoogleMapView
 import ch.epfl.sdp.cook4me.ui.map.dummyMarkers
+import ch.epfl.sdp.cook4me.ui.navigation.BottomNavigationBar
 import ch.epfl.sdp.cook4me.ui.profile.EditProfileScreen
 import ch.epfl.sdp.cook4me.ui.profile.PostDetails
 import ch.epfl.sdp.cook4me.ui.profile.ProfileScreen
@@ -60,6 +90,80 @@ val testEvent = Event(
     isPrivate = false
 )
 
+sealed class BottomNavScreen(val route: String, val icon: ImageVector, val title: String) {
+    object Home : BottomNavScreen(Screen.CreateTupperwareScreen.name, Icons.Filled.Home, "Home")
+    object Map : BottomNavScreen(Screen.Map.name, Icons.Filled.LocationOn, "Map")
+    object Profile : BottomNavScreen(Screen.ProfileScreen.name, Icons.Filled.Person, "Profile")
+    object Menu: BottomNavScreen(Screen.OverviewScreen.name, Icons.Filled.Menu, "Menu")
+}
+
+@Composable
+fun Cook4MeApp() {
+    // User authentication
+    val auth: FirebaseAuth = FirebaseAuth.getInstance()
+    val isAuthenticated = remember { mutableStateOf(auth.currentUser != null) }
+
+    // Navigation controller
+    val navController = rememberNavController()
+
+    val startScreen: String = if (isAuthenticated.value) {
+        BottomNavScreen.Map.route
+    } else {
+        Screen.Login.name
+    }
+
+    val navGraph = navController.createGraph(startDestination = startScreen) {
+        composable(BottomNavScreen.Home.route) { CreateTupperwareScreen() }
+        composable(BottomNavScreen.Map.route) { GoogleMapView(modifier = Modifier.fillMaxSize(), markers = dummyMarkers) }
+        composable(BottomNavScreen.Profile.route) { ProfileScreen() }
+        composable(BottomNavScreen.Menu.route) {
+            OverviewScreen(
+                onMapClick = { navController.navigate(Screen.Map.name) },
+                onProfileClick = { navController.navigate(Screen.ProfileScreen.name) },
+                onEditProfileClick = { navController.navigate(Screen.EditProfileScreen.name) },
+                onAddTupperwareClick = { navController.navigate(Screen.CreateTupperwareScreen.name) },
+                onSwipeTupperwareClick = { navController.navigate(Screen.TupperwareSwipeScreen.name) },
+                onAddEventClick = { navController.navigate(Screen.CreateEventScreen.name) },
+                onAddSignUpClick = { navController.navigate(Screen.SignUpScreen.name) },
+                onPostClick = { navController.navigate(Screen.PostDetails.name) },
+                onDetailedEventClick = { navController.navigate(Screen.DetailedEventScreen.name) },
+                onAddRecipeClick = { navController.navigate(Screen.CreateRecipeScreen.name) },
+            )
+        }
+        composable(route = Screen.EditProfileScreen.name) { EditProfileScreen() }
+        composable(route = Screen.TupperwareSwipeScreen.name) { TupperwareSwipeScreen() }
+        composable(route = Screen.CreateEventScreen.name) { CreateEventScreen() }
+        composable(route = Screen.DetailedEventScreen.name) { DetailedEventScreen(event = testEvent) }
+        composable(route = Screen.SignUpScreen.name) { SignUpScreen() }
+        composable(route = Screen.CreateRecipeScreen.name) { CreateRecipeScreen(submitForm = {}) }
+        composable(route = Screen.PostDetails.name) {
+            val post = Post(1, "Tiramisu", "This is a delicious triamisu or so")
+            PostDetails(data = post, painter = painterResource(R.drawable.tiramisu))
+        }
+        composable(route = Screen.Login.name) {
+            LoginScreen(
+                onSuccessfulLogin = {
+                    isAuthenticated.value = true
+                    navController.navigate(BottomNavScreen.Map.route) {
+                        popUpTo(Screen.Login.name) { inclusive = true } // This popUp blocks the user being able to go back once logged in
+                    }
+                }
+            )
+        }
+    }
+    if (isAuthenticated.value) {
+        Scaffold(
+            bottomBar = { BottomNavigationBar(navController) }
+
+        ) { scaffoldPadding ->
+            NavHost(navController = navController, graph = navGraph, modifier = Modifier.padding(scaffoldPadding))
+        }
+    } else {
+        NavHost(navController = navController, graph = navGraph)
+    }
+}
+
+/*
 @Composable
 fun Cook4MeApp(
     navController: NavHostController = rememberNavController()
@@ -129,3 +233,4 @@ fun Cook4MeApp(
         }
     }
 }
+*/
---
+++ app/src/main/java/ch/epfl/sdp/cook4me/ui/navigation/BottomNavigationBar.kt
@@ -0,0 +1,56 @@
+package ch.epfl.sdp.cook4me.ui.navigation
+
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import ch.epfl.sdp.cook4me.BottomNavScreen
+
+@Composable
+fun BottomNavigationBar(navController: NavHostController = rememberNavController()) {
+    val screens = listOf(
+        BottomNavScreen.Home,
+        BottomNavScreen.Map,
+        BottomNavScreen.Profile,
+        BottomNavScreen.Menu
+    )
+
+    BottomNavigation(
+        backgroundColor = MaterialTheme.colors.background,
+        elevation = 8.dp
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        screens.forEach { screen ->
+            BottomNavigationItem(
+                icon = { Icon(screen.icon, contentDescription = null) },
+                label = { Text(screen.title) },
+                selected = currentRoute == screen.route,
+                onClick = {
+                    navController.navigate(screen.route) {
+                        // Pop up to the start destination of the graph to
+                        // avoid building up a large stack of destinations
+                        // on the back stack as users select items
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        // Avoid multiple copies of the same destination when
+                        // re-selecting the same item
+                        launchSingleTop = true
+                        // Restore state when navigating back
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
