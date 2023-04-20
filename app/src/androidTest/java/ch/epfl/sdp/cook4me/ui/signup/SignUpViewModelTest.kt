c59e38660128dfa6ec51fa33f21d2abe2666335d
nino-gerb <nino.gerber@epfl.ch>
2023-04-20 23:07:33 +0200

    pump up coverage

--- app/src/androidTest/java/ch/epfl/sdp/cook4me/ui/signup/SignUpViewModelTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/cook4me/ui/signup/SignUpViewModelTest.kt
@@ -1,15 +1,52 @@
 package ch.epfl.sdp.cook4me.ui.signup
 
+import android.content.Context
 import androidx.activity.ComponentActivity
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.core.net.toUri
+import androidx.test.platform.app.InstrumentationRegistry
 import ch.epfl.sdp.cook4me.ui.signUp.SignUpViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
 class SignUpViewModelTest {
     @get:Rule
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+    private lateinit var auth: FirebaseAuth
+    private lateinit var context: Context
+    private lateinit var firestore: FirebaseFirestore
+
+    @Before
+    fun setUp() {
+        context = InstrumentationRegistry.getInstrumentation().targetContext
+        /*
+        * IMPORTANT:
+        * (Below code is already functional, no need to change anything)
+        * Make sure you do this try-catch block,
+        * otherwise when doing CI, there will be an exception:
+        * kotlin.UninitializedPropertyAccessException: lateinit property firestore has not been initialized
+        * */
+        try {
+            Firebase.firestore.useEmulator("10.0.2.2", 8080)
+        } catch (e: IllegalStateException) {
+            // emulator already set
+            // do nothing
+        }
+        try {
+            Firebase.auth.useEmulator("10.0.2.2", 9099)
+        } catch (e: IllegalStateException) {
+            // emulator already set
+            // do nothing
+        }
+        firestore = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
+    }
 
     @Test
     fun testUserIsCorrect() {
@@ -48,6 +85,8 @@ class SignUpViewModelTest {
 
         // check that its valid after adding it
         assert(signUpViewModel.checkForm())
+
+        signUpViewModel.onSubmit()
     }
 
     @Test
