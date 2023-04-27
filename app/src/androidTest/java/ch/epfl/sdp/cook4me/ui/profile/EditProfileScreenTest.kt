b56e2baa5eaa5c0412d1ee0f9dcd1997553ea410
nino-gerb <nino.gerber@epfl.ch>
2023-04-27 23:13:29 +0200

    cirrus lint & dedekt

--- app/src/androidTest/java/ch/epfl/sdp/cook4me/ui/profile/EditProfileScreenTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/cook4me/ui/profile/EditProfileScreenTest.kt
@@ -5,10 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.performTextClearance
-import androidx.compose.ui.test.performTextInput
 import androidx.test.platform.app.InstrumentationRegistry
 import ch.epfl.sdp.cook4me.R
 import ch.epfl.sdp.cook4me.persistence.model.Profile
