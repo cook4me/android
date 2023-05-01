8f81ed1c91b2ab860f2a73b792c4139524f91263
bu-da <daniel.bucher@healthinal.com>
2023-05-01 15:56:12 +0200

    add very basic match screen placeholder

---
+++ app/src/main/java/ch/epfl/sdp/cook4me/ui/tupperwareswipe/MatchScreen.kt
@@ -0,0 +1,18 @@
+package ch.epfl.sdp.cook4me.ui.tupperwareswipe
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MatchScreen(
+
+) {
+    Text("Its a match")
+}
