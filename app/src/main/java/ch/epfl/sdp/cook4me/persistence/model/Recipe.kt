22673ec6b9213a892c6bc10c6314dbbc8ec5f4e4
dayan9265 <82887324+dayan9265@users.noreply.github.com>
2023-05-03 16:26:16 +0200

    bypass equality problem when testing due to creationTime

--- app/src/main/java/ch/epfl/sdp/cook4me/persistence/model/Recipe.kt
+++ app/src/main/java/ch/epfl/sdp/cook4me/persistence/model/Recipe.kt
@@ -15,4 +15,20 @@ data class Recipe(
     val servings: Int = 0,
     val cookingTime: String = "",
     val creationTime: Timestamp = Timestamp.now(),
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (other === this) return true
+        if (other !is Recipe) return false
+        // if all fields same except creationTime, then they are same
+        // since creationTime is modified when recipe is added to firestore
+        return user == other.user &&
+                name == other.name &&
+                id == other.id &&
+                ingredients == other.ingredients &&
+                recipeSteps == other.recipeSteps &&
+                photos == other.photos &&
+                difficulty == other.difficulty &&
+                servings == other.servings &&
+                cookingTime == other.cookingTime
+    }
+}
