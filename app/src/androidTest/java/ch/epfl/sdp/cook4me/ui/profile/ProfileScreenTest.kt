b2c8841fa7dc35f50ea5e6b98754b57c13638ef1
nino-gerb <nino.gerber@epfl.ch>
2023-05-04 01:31:07 +0200

    remove unused import

--- app/src/androidTest/java/ch/epfl/sdp/cook4me/ui/profile/ProfileScreenTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/cook4me/ui/profile/ProfileScreenTest.kt
@@ -12,7 +12,6 @@ import ch.epfl.sdp.cook4me.persistence.repository.ProfileImageRepository
 import ch.epfl.sdp.cook4me.persistence.repository.ProfileRepository
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.runBlocking
