2c88da4f0cb6365e29bd7e439235c035ec167dcc
Tachi-67 <haolongli_neko@outlook.com>
2023-05-08 17:44:26 +0200

    add back comments because removing is not helpful to codeclimate

--- app/src/main/java/ch/epfl/sdp/cook4me/application/ChallengeFormService.kt
+++ app/src/main/java/ch/epfl/sdp/cook4me/application/ChallengeFormService.kt
@@ -13,6 +13,11 @@ class ChallengeFormService(
         ObjectCollectionRepository(objectPath = CHALLENGE_PATH)
 ) {
 
+    /**
+     * Submits the form if it is valid, otherwise returns the error message
+     * @param challenge the challenge to submit
+     * @return null if the challenge is valid, the error message otherwise
+     */
     suspend fun submitForm(challenge: Challenge): String? = if (challenge.isValidChallenge) {
         objectRepository.add(challenge)
         null
@@ -20,21 +25,41 @@ class ChallengeFormService(
         challenge.challengeProblem
     }
 
+    /*
+    * Retrieves challenge with query name at the given field
+    * e.g. getWithGivenField("name", "darth.vadar") will return a map
+    *     of challenges with name (challenge attr.) "darth.vadar"
+    * map id: the id of the challenge, map value: the challenge object
+    * When nothing is found, an empty map is returned
+    * */
     suspend fun getWithGivenField(field: String, query: Any): Map<String, Challenge> {
         val result = objectRepository.getWithGivenField<Challenge>(field, query)
         return result.map { it.id to documentSnapshotToChallenge(it) }.toMap()
     }
 
+    /*
+    * To get the challenge of given id.
+    * If nothing is found, null is returned
+    * */
     suspend fun getChallengeWithId(id: String): Challenge? {
         val result = objectRepository.getWithId<Challenge>(id)
         return result?.let { documentSnapshotToChallenge(it) }
     }
 
+    /*
+    * Retrieve all challenges in a Map
+    * */
     suspend fun retrieveAllChallenges(): Map<String, Challenge> {
         val result = objectCollectionRepository.retrieveAllDocuments<Challenge>()
         return result.map { it.id to documentSnapshotToChallenge(it) }.toMap()
     }
 
+    /*
+    * Notes: Firebase could not serialize to java.untl.Calender, I will add an constructor in Challenge.kt
+    * to construct an Challenge object from a map.
+    * This function is used to convert a document snapshot to an challenge object manually.
+    * Also see: Challenge.kt
+    * */
     private fun documentSnapshotToChallenge(documentSnapshot: DocumentSnapshot) =
         Challenge(documentSnapshot.data ?: emptyMap())
 }
