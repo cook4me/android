e43e4adc3e7a0c82bcbc79cd6213e47c7df30dfd
pauromeu <romeu.pau@gmail.com>
2023-05-16 15:43:15 +0200

    update marker with location picker

--- app/src/main/java/ch/epfl/sdp/cook4me/ui/map/LocationPicker.kt
+++ app/src/main/java/ch/epfl/sdp/cook4me/ui/map/LocationPicker.kt
@@ -1,6 +1,8 @@
 package ch.epfl.sdp.cook4me.ui.map
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -9,12 +11,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ch.epfl.sdp.cook4me.ui.map.Locations.EPFL
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
-import com.google.maps.android.compose.rememberMarkerState
 
 private const val ZOOM_DEFAULT_VALUE = 15f
 
@@ -22,27 +26,26 @@ private const val ZOOM_DEFAULT_VALUE = 15f
 fun LocationPicker(
     onLocationPicked: (LatLng) -> Unit
 ) {
-    var markerPosition by remember { mutableStateOf<LatLng?>(null) }
-    var markers by remember { mutableStateOf<List<LatLng?>>(emptyList()) }
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(EPFL, ZOOM_DEFAULT_VALUE)
     }
-    GoogleMap(
-        modifier = Modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState,
-        onMapClick = { latLng ->
-            markerPosition = null
-            onLocationPicked(latLng)
-            cameraPositionState.position = CameraPosition.fromLatLngZoom(latLng, ZOOM_DEFAULT_VALUE)
-            markerPosition = latLng
-        },
-    ) {
-        markerPosition?.let { position ->
-            val markerState = rememberMarkerState(position = position)
-            Marker(
-                state = markerState,
-                draggable = true
-            )
+    var markerState by remember { mutableStateOf<MarkerState?>(null) }
+    Column {
+        if (markerState == null) {
+            Text("Click on the map to pick a location")
+        }
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState,
+            onMapClick = { latLng ->
+                onLocationPicked(latLng)
+                markerState = MarkerState(position = latLng)
+                cameraPositionState.move(CameraUpdateFactory.newLatLngZoom(latLng, ZOOM_DEFAULT_VALUE))
+            },
+        ) {
+            markerState?.let { state ->
+                Marker(state = state)
+            }
         }
     }
 }
