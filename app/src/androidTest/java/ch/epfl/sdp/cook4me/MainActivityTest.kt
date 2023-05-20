10431858de3b502715bc7104510d3dbc4a3f049a
bu-da <daniel.bucher@healthinal.com>
2023-05-20 12:08:30 +0200

    add smoke test and placeholder unit test back

--- app/src/androidTest/java/ch/epfl/sdp/cook4me/MainActivityTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/cook4me/MainActivityTest.kt
@@ -1,12 +1,19 @@
 package ch.epfl.sdp.cook4me
 
-// TODO: is this still needed
-// class MainActivityTest {
-//    @get:Rule val rule = ActivityScenarioRule(MainActivity::class.java)
-//
-//    @Test
-//    fun activity_getsCreated() {
-//        rule.scenario.moveToState(Lifecycle.State.CREATED)
-//        assertTrue(rule.scenario.state.isAtLeast(Lifecycle.State.CREATED))
-//    }
-// }
+import androidx.lifecycle.Lifecycle
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Assert.assertTrue
+import org.junit.Rule
+import org.junit.Test
+
+// TODO: is this still needed?
+class MainActivityTest {
+    @get:Rule
+    val rule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun activity_getsCreated() {
+        rule.scenario.moveToState(Lifecycle.State.CREATED)
+        assertTrue(rule.scenario.state.isAtLeast(Lifecycle.State.CREATED))
+    }
+}
