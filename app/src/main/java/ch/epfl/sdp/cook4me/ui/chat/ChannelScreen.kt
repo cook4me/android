3f797ac08484b978bf33a56397425123f0e94395
bu-da <daniel.bucher@healthinal.com>
2023-05-20 16:06:19 +0200

    sketch a draft how we can profile images

--- app/src/main/java/ch/epfl/sdp/cook4me/ui/chat/ChannelScreen.kt
+++ app/src/main/java/ch/epfl/sdp/cook4me/ui/chat/ChannelScreen.kt
@@ -1,8 +1,10 @@
 package ch.epfl.sdp.cook4me.ui.chat
 
+import android.content.Context
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -12,13 +14,24 @@ import androidx.core.content.ContextCompat.startActivity
 import ch.epfl.sdp.cook4me.BuildConfig
 import ch.epfl.sdp.cook4me.R
 import ch.epfl.sdp.cook4me.application.AccountService
+import ch.epfl.sdp.cook4me.persistence.repository.ProfileImageRepository
 import ch.epfl.sdp.cook4me.ui.common.LoadingScreen
 import io.getstream.chat.android.client.ChatClient
 import io.getstream.chat.android.client.models.Filters
 import io.getstream.chat.android.client.models.User
+import coil.ComponentRegistry
+import coil.ImageLoader
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import coil.request.DefaultRequestOptions
+import coil.request.Disposable
+import coil.request.ImageRequest
+import coil.request.ImageResult
+import com.getstream.sdk.chat.coil.StreamImageLoaderFactory
 import io.getstream.chat.android.compose.ui.channels.ChannelsScreen
 import io.getstream.chat.android.compose.ui.theme.ChatTheme
-import kotlinx.coroutines.runBlocking
+import io.getstream.chat.android.compose.ui.util.StreamCoilImageLoaderFactory
+
 
 // TODO: Refactor needed: https://github.com/cook4me/android/issues/155
 @Composable
@@ -30,11 +43,7 @@ fun ChannelScreen(
     accountService: AccountService = AccountService(),
     onBackListener: () -> Unit = {},
 ) {
-    // disconnecting the client before connecting again, otherwise will
-    // cause error: too many connections
-    runBlocking {
-        client.disconnect(true).enqueue()
-    }
+
     val context = LocalContext.current
     val userEmail = accountService.getCurrentUserWithEmail()
     val fullName = remember { mutableStateOf("") }
@@ -42,19 +51,28 @@ fun ChannelScreen(
     val user = remember {
         mutableStateOf(User(id = fullName.value))
     }
-    // The user email is always not null, it's a bit of boilerplate.
-    userEmail?.let { email ->
-        // parsing email to get the name (user id)
-        val nameParts = email.split("@")[0].replace(".", "")
-        fullName.value = nameParts.trim()
-        user.value = User(id = fullName.value)
-        // generating user token and connecting the user
-        val token = client.devToken(user.value.id)
-        client.connectUser(user.value, token).enqueue { result ->
-            if (result.isSuccess) {
-                isConnected.value = true
-            } else {
-                println("connection not successful")
+
+    // TODO: I would have preferred to use LaunchedEffect instead of a callback chain
+    client.disconnect(true).enqueue { disconnectResult ->
+        if (disconnectResult.isSuccess) {
+            // disconnecting the client before connecting again, otherwise will
+            // cause error: too many connections
+            // The user email is always not null, it's a bit of boilerplate.
+            userEmail?.let { email ->
+                // parsing email to get the name (user id)
+                val nameParts = email.split("@")[0].replace(".", "")
+                fullName.value = nameParts.trim()
+                user.value = User(id = fullName.value, image = "${PROFILE_IMAGE_PREFIX}$email")
+                // generating user token and connecting the user
+                val token = client.devToken(user.value.id)
+                client.connectUser(user.value, token).enqueue { result ->
+                    if (result.isSuccess) {
+                        isConnected.value = true
+                    } else {
+                        //TODO: use Log.e(...) instead
+                        println("connection not successful")
+                    }
+                }
             }
         }
     }
@@ -65,7 +83,7 @@ fun ChannelScreen(
             .testTag(context.getString(R.string.Channel_Screen_Tag))
     ) {
         if (isConnected.value) {
-            ChatTheme {
+            ChatTheme(imageLoaderFactory = CoilImageLoaderFactory) {
                 ChannelsScreen(
                     filters = Filters.and(
                         Filters.eq("type", "messaging"),
@@ -80,9 +98,7 @@ fun ChannelScreen(
                         startActivity(context, intent, null)
                     },
                     onBackPressed = { onBackListener() },
-                    onHeaderAvatarClick = {
-                        client.disconnect(true).enqueue()
-                    },
+                    isShowingHeader = false
                 )
             }
         } else {
