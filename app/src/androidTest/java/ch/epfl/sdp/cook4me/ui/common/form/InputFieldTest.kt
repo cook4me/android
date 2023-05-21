bea89a07230eb084088183bd037ed772dbbfbe5a
bu-da <daniel.bucher@healthinal.com>
2023-05-21 17:25:50 +0200

    add back common form tests and reorder them

---
+++ app/src/androidTest/java/ch/epfl/sdp/cook4me/ui/common/form/InputFieldTest.kt
@@ -0,0 +1,50 @@
+package ch.epfl.sdp.cook4me.ui.common.form
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import ch.epfl.sdp.cook4me.R
+import ch.epfl.sdp.cook4me.ui.onNodeWithStringId
+import org.junit.Assert.assertEquals
+
+@RunWith(AndroidJUnit4::class)
+ class InputFieldTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun questionLabelAndExampleTextIsDisplayed() {
+        composeTestRule.setContent {
+            InputField(
+                question = R.string.question, label = R.string.label,
+                value = "",
+                onValueChange = {}
+            )
+        }
+
+        composeTestRule.onNodeWithStringId(R.string.question).assertIsDisplayed()
+        composeTestRule.onNodeWithStringId(R.string.label).assertIsDisplayed()
+    }
+
+    @Test
+    fun onTextChangedIsCalledWhenTextIsChanged() {
+        var text = ""
+        composeTestRule.setContent {
+            InputField(
+                question = R.string.question, label = R.string.label,
+                onValueChange = { text = it },
+                value = ""
+            )
+        }
+
+        composeTestRule.onNodeWithText("").performTextInput("new text")
+        assertEquals("new text", text)
+    }
+ }
---
+++ app/src/androidTest/java/ch/epfl/sdp/cook4me/ui/common/form/TimePickerTest.kt
@@ -0,0 +1,28 @@
+package ch.epfl.sdp.cook4me.ui.common.form
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import ch.epfl.sdp.cook4me.ui.onNodeWithStringId
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import ch.epfl.sdp.cook4me.R
+
+@RunWith(AndroidJUnit4::class)
+ class TimePickerTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun defaultInformationIsDisplayed() {
+        composeTestRule.setContent {
+            TimePicker(onTimeChanged = {})
+        }
+
+        composeTestRule.onNodeWithStringId(R.string.select_time_button).assertIsDisplayed()
+        composeTestRule.onNodeWithStringId(R.string.selected_time_text).assertIsDisplayed()
+    }
+ }
