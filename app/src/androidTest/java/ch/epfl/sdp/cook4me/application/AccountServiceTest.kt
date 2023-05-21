12ede02735f9660ac0731b7f83be2f6342e9f109
bu-da <daniel.bucher@healthinal.com>
2023-05-21 14:57:15 +0200

    add AccountService tests back

--- app/src/androidTest/java/ch/epfl/sdp/cook4me/application/AccountServiceTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/cook4me/application/AccountServiceTest.kt
@@ -1,92 +1,113 @@
 package ch.epfl.sdp.cook4me.application
 
-// @ExperimentalCoroutinesApi
-// @RunWith(AndroidJUnit4::class)
-// class AccountServiceTest {
-//    @get:Rule
-//    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
-//    private lateinit var auth: FirebaseAuth
-//    private lateinit var accountService: AccountService
-//    private val invalidEmail = "invalidemail"
-//    private val validEmail = "bababa@epfl.ch"
-//
-//    @Before
-//    fun setUp() {
-//        Firebase.auth.useEmulator("10.0.2.2", 9099)
-//        auth = FirebaseAuth.getInstance()
-//        runBlocking {
-//            auth.createUserWithEmailAndPassword("harry.potter@epfl.ch", "123456").await()
-//        }
-//        accountService = AccountService(auth)
-//    }
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import ch.epfl.sdp.cook4me.assertThrowsAsync
+import ch.epfl.sdp.cook4me.setupFirebaseAuth
+import com.google.firebase.auth.FirebaseAuth
+import junit.framework.TestCase.assertNull
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.test.runTest
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
-//    @After
-//    fun cleanUp() {
-//        runBlocking {
-//            auth.signInWithEmailAndPassword("harry.potter@epfl.ch", "123456").await()
-//            auth.currentUser?.delete()
-//        }
-//    }
+private const val USERNAME = "harry.potter@epfl.ch"
+private const val PASSWORD = "123456"
 
-//    @Test
-//    fun accountServiceSignsOutCurrentUser() = runTest {
-//        accountService.authenticate("harry.potter@epfl.ch", "123456")
-//        accountService.signOut()
-//        assertNull(auth.currentUser)
-//    }
+private const val VALID_EMAIL = "bababa@epfl.ch"
+private const val INVALID_EMAIL = "bababa@epfl.ch"
 
-//    @Test
-//    fun accountServiceRegistersValidUser() = runTest {
-//        accountService.register("validemail@epfl.ch", "123456")
-//        accountService.authenticate("validemail@epfl.ch", "123456")
-//        assertThat(auth.currentUser?.email, `is`("validemail@epfl.ch"))
-//        auth.currentUser?.delete()
-//    }
 
-//    @Test
-//    fun accountServiceRefusesToRegisterInvalidEmail() = runTest {
-//        assertThrowsAsync {
-//            accountService.register("mrinvalid", "123456")
-//        }
-//    }
+@ExperimentalCoroutinesApi
+ @RunWith(AndroidJUnit4::class)
+ class AccountServiceTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+    private val auth: FirebaseAuth = setupFirebaseAuth()
+    private val accountService: AccountService = AccountService(auth)
 
-//    @Test
-//    fun accountServiceRefusesToRegisterWeakPassword() = runTest {
-//        assertThrowsAsync {
-//            accountService.register("validemail@epfl.ch", "126")
-//        }
-//    }
+    @Before
+    fun setUp() {
+        runBlocking {
+            auth.createUserWithEmailAndPassword(USERNAME, PASSWORD).await()
+        }
+    }
 
-//    @Test
-//    fun accountServiceRefusesToRegisterExistingUser() = runTest {
-//        accountService.register("validemail@epfl.ch", "123456")
-//        assertThrowsAsync {
-//            accountService.register("validemail@epfl.ch", "123456")
-//        }
-//        accountService.authenticate("validemail@epfl.ch", "123456")
-//        auth.currentUser?.delete()
-//    }
+    @After
+    fun cleanUp() {
+        runBlocking {
+            auth.signInWithEmailAndPassword(USERNAME, PASSWORD).await()
+            auth.currentUser?.delete()?.await()
+        }
+    }
 
-//    @Test
-//    fun accountServicePermitsValidEmail() {
-//        assertThat(accountService.isValidEmail(validEmail), `is`(true))
-//    }
+    @Test
+    fun accountServiceSignsOutCurrentUser() = runTest {
+        accountService.authenticate(USERNAME, PASSWORD)
+        accountService.signOut()
+        assertNull(auth.currentUser)
+    }
 
-//    @Test
-//    fun accountServiceDeclinesInvalidEmail() {
-//        assertThat(accountService.isValidEmail(invalidEmail), `is`(false))
-//    }
+    @Test
+    fun accountServiceRegistersValidUser() = runTest {
+        accountService.register(VALID_EMAIL, PASSWORD)
+        accountService.authenticate(VALID_EMAIL, PASSWORD)
+        assertThat(auth.currentUser?.email, `is`(VALID_EMAIL))
+        auth.currentUser?.delete()?.await()
+    }
 
-//    @Test
-//    fun accountServicePermitsSignedUpUser() = runTest {
-//        accountService.authenticate("harry.potter@epfl.ch", "123456")
-//        assertThat(auth.currentUser?.email, `is`("harry.potter@epfl.ch"))
-//    }
+    @Test
+    fun accountServiceRefusesToRegisterInvalidEmail() = //Don't use runTest, because we already use runBlocking
+        assertThrowsAsync {
+            accountService.register("mrinvalid", PASSWORD)
+        }
 
-//    @Test
-//    fun accountServiceDeclinesInvalidUser() = runTest {
-//        assertThrowsAsync {
-//            accountService.authenticate("mrinvalid@epfl.ch", "hahaha")
-//        }
-//    }
-// }
+    @Test
+    fun accountServiceRefusesToRegisterWeakPassword() =
+        assertThrowsAsync {
+            accountService.register("validemail@epfl.ch", "126")
+        }
+
+    @Test
+    fun accountServiceRefusesToRegisterExistingUser() {
+        assertThrowsAsync {
+            accountService.register("validemail@epfl.ch", "123456")
+            accountService.register("validemail@epfl.ch", "123456")
+        }
+        runBlocking {
+            accountService.authenticate("validemail@epfl.ch", "123456")
+            auth.currentUser?.delete()?.await()
+        }
+    }
+
+    @Test
+    fun accountServicePermitsValidEmail() {
+        assertThat(accountService.isValidEmail(VALID_EMAIL), `is`(true))
+    }
+
+    @Test
+    fun accountServiceDeclinesInvalidEmail() {
+        assertThat(accountService.isValidEmail(INVALID_EMAIL), `is`(false))
+    }
+
+    @Test
+    fun accountServicePermitsSignedUpUser() = runTest {
+        accountService.authenticate(USERNAME, PASSWORD)
+        assertThat(auth.currentUser?.email, `is`(USERNAME))
+    }
+
+    @Test
+    fun accountServiceDeclinesInvalidUser() =
+        assertThrowsAsync {
+            accountService.authenticate("mrinvalid@epfl.ch", "hahaha")
+        }
+
+ }
