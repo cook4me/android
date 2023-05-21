97e80cc4860239216e4a5be4a7b1ef3acbcc22ea
bu-da <daniel.bucher@healthinal.com>
2023-05-21 17:02:33 +0200

    add chat tests back

--- app/src/androidTest/java/ch/epfl/sdp/cook4me/ui/chat/MessagesActivityTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/cook4me/ui/chat/MessagesActivityTest.kt
@@ -1,14 +1,21 @@
 package ch.epfl.sdp.cook4me.ui.chat
 
-// @RunWith(AndroidJUnit4::class)
-// class MessagesActivityTest {
-//    @Test
-//    fun testMessagesActivityFinishesWhenNullChannelIdIsPassed() {
-//        val context = androidx.test.platform.app.InstrumentationRegistry.getInstrumentation().targetContext
-//        val intent = Intent(context, MessagesActivity::class.java)
-//
-//        ActivityScenario.launchActivityForResult<MessagesActivity>(intent).use { scenario ->
-//            assert(scenario.result.resultCode == Activity.RESULT_CANCELED)
-//        }
-//    }
-// }
+import android.app.Activity
+import android.content.Intent
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+ class MessagesActivityTest {
+    @Test
+    fun testMessagesActivityFinishesWhenNullChannelIdIsPassed() {
+        val context = androidx.test.platform.app.InstrumentationRegistry.getInstrumentation().targetContext
+        val intent = Intent(context, MessagesActivity::class.java)
+
+        ActivityScenario.launchActivityForResult<MessagesActivity>(intent).use { scenario ->
+            assert(scenario.result.resultCode == Activity.RESULT_CANCELED)
+        }
+    }
+ }
