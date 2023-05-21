7b4d2795f8958d3291912852638697ab645ed758
bu-da <daniel.bucher@healthinal.com>
2023-05-21 16:09:28 +0200

    use again toMap function instead of object

--- app/src/main/java/ch/epfl/sdp/cook4me/persistence/repository/ChallengeRepository.kt
+++ app/src/main/java/ch/epfl/sdp/cook4me/persistence/repository/ChallengeRepository.kt
@@ -2,12 +2,13 @@ package ch.epfl.sdp.cook4me.persistence.repository
 
 import ch.epfl.sdp.cook4me.ui.challenge.Challenge
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
 
 private const val COLLECTION_PATH = "challenges"
 
 class ChallengeRepository(private val store: FirebaseFirestore = FirebaseFirestore.getInstance()) {
     suspend fun add(challenge: Challenge) =
-        store.addObjectToCollection(challenge, COLLECTION_PATH)
+        store.collection(COLLECTION_PATH).add(challenge.toMap()).await().id
 
     suspend fun getById(id: String) =
         store.getObjectByIdFromCollection(id, COLLECTION_PATH) {
