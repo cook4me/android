8de23074fb2e19adadf4d359bca8ac75cf39b392
bu-da <daniel.bucher@healthinal.com>
2023-05-22 14:05:30 +0200

    add comment with issue url

--- app/src/androidTest/java/ch/epfl/sdp/cook4me/ui/map/MapPermissionWrapper.kt
+++ app/src/androidTest/java/ch/epfl/sdp/cook4me/ui/map/MapPermissionWrapper.kt
@@ -1,46 +1,43 @@
 package ch.epfl.sdp.cook4me.ui.map
 
-// import androidx.compose.foundation.layout.fillMaxSize
-// import androidx.compose.ui.Modifier
-// import androidx.compose.ui.test.assertIsDisplayed
-// import androidx.compose.ui.test.junit4.createComposeRule
-// import androidx.compose.ui.test.onNodeWithText
-// import ch.epfl.sdp.cook4me.permissions.TestPermissionStatusProvider
-// import org.junit.Rule
-// import org.junit.Test
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.runner.RunWith
 
-// class TestMapPermissionWrapper {
-//    @get:Rule
-//    val composeTestRule = createComposeRule()
+@RunWith(AndroidJUnit4::class)
+class TestMapPermissionWrapper {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
-// TODO: Fix the tests (potentially related to the network permission)
-    /*
-    @Test
-    fun assertMapIsDisplayedWhenLocationIsEnabled() {
-        val permissionStatusProvider = TestPermissionStatusProvider(mapOf("location" to Pair(true, false)))
-
-        composeTestRule.setContent {
-            MapPermissionWrapper(
-                permissionStatusProvider = permissionStatusProvider,
-                modifier = Modifier.fillMaxSize()
-            )
-        }
-
-        composeTestRule.onNodeWithText("EPFL").assertIsDisplayed() // Map is shown
-    }
-
-    @Test
-    fun assertMapIsNotDisplayedWhenLocationIsNotEnabled() {
-        val permissionStatusProvider = TestPermissionStatusProvider(mapOf("location" to Pair(false, false)))
-
-        composeTestRule.setContent {
-            MapPermissionWrapper(
-                permissionStatusProvider = permissionStatusProvider,
-                modifier = Modifier.fillMaxSize(),
-            )
-        }
-
-        composeTestRule.onNodeWithText("The location permission will grant a better experience in the app").assertIsDisplayed()
-    }
-    */
-// }
+// TODO: Fix the tests (potentially related to the network permission), https://github.com/cook4me/android/issues/250
+//
+//    @Test
+//    fun assertMapIsDisplayedWhenLocationIsEnabled() {
+//        val permissionStatusProvider = TestPermissionStatusProvider(mapOf("location" to Pair(true, false)))
+//
+//        composeTestRule.setContent {
+//            MapPermissionWrapper(
+//                permissionStatusProvider = permissionStatusProvider,
+//                modifier = Modifier.fillMaxSize()
+//            )
+//        }
+//
+//        composeTestRule.onNodeWithText("EPFL").assertIsDisplayed() // Map is shown
+//    }
+//
+//    @Test
+//    fun assertMapIsNotDisplayedWhenLocationIsNotEnabled() {
+//        val permissionStatusProvider = TestPermissionStatusProvider(mapOf("location" to Pair(false, false)))
+//
+//        composeTestRule.setContent {
+//            MapPermissionWrapper(
+//                permissionStatusProvider = permissionStatusProvider,
+//                modifier = Modifier.fillMaxSize(),
+//            )
+//        }
+//
+//        composeTestRule.onNodeWithText("The location permission will grant a better experience in the app").assertIsDisplayed()
+//    }
+}
