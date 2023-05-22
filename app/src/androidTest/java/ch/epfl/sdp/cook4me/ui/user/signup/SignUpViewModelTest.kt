b27f6b8fb9a8f5610ba750c9fd750c79012ef709
bu-da <daniel.bucher@healthinal.com>
2023-05-22 10:53:43 +0200

    add SignUpViewModel tests back

--- app/src/androidTest/java/ch/epfl/sdp/cook4me/ui/user/signup/SignUpViewModelTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/cook4me/ui/user/signup/SignUpViewModelTest.kt
@@ -1,178 +1,175 @@
 package ch.epfl.sdp.cook4me.ui.user.signup
 
-private val COLLECTION_PATH_PROFILES = "profiles"
+import android.annotation.SuppressLint
+import android.net.Uri
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import ch.epfl.sdp.cook4me.persistence.repository.ProfileImageRepository
+import ch.epfl.sdp.cook4me.persistence.repository.ProfileRepository
+import ch.epfl.sdp.cook4me.setupFirebaseAuth
+import ch.epfl.sdp.cook4me.setupFirebaseStorage
+import ch.epfl.sdp.cook4me.setupFirestore
+import ch.epfl.sdp.cook4me.ui.user.profile.ProfileViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.tasks.await
+import org.junit.After
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
-// class SignUpViewModelTest {
-//    @get:Rule
-//    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
-//
-//    private lateinit var profileImageRepository: ProfileImageRepository
-//    private lateinit var store: FirebaseFirestore
-//    private lateinit var storage: FirebaseStorage
-//    private lateinit var auth: FirebaseAuth
-//
-//    // Set input
-//    private val username = "donald"
-//    private val favoriteDish = "pizza"
-//    private val allergies = "gluten"
-//    private val bio = "I love cooking"
-//    private val email = "donald.duck@epfl.ch"
-//    private val password = "123456"
-//
-//    private val userImage: Uri =
-//        Uri.parse("android.resource://ch.epfl.sdp.cook4me/drawable/ic_launcher_foreground")
-//
-//    @Before
-//    fun setUp() {
-//        // Connect to local firestore emulator
-//        store = FirebaseFirestore.getInstance()
-//        val settings = FirebaseFirestoreSettings.Builder()
-//            .setHost("10.0.2.2:8080") // connect to local firestore emulator
-//            .setSslEnabled(false)
-//            .setPersistenceEnabled(false)
-//            .build()
-//        store.firestoreSettings = settings
-//        Firebase.auth.useEmulator("10.0.2.2", 9099)
-//        storage = FirebaseStorage.getInstance()
-//        storage.useEmulator("10.0.2.2", 9199)
-//        auth = FirebaseAuth.getInstance()
-//        profileImageRepository = ProfileImageRepository(store, storage, auth)
-//    }
+private const val USERNAME = "donald"
+private const val PASSWORD = "123456"
+private const val FAVORITE_DISH = "pizza"
+private const val ALLERGIES = "gluten"
+private const val BIO = "I love cooking"
+private const val EMAIL = "donald.duck@epfl.ch"
+@RunWith(AndroidJUnit4::class)
+ class SignUpViewModelTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
-//    @After
-//    fun cleanUp() {
-//        runBlocking {
-//            auth.signInWithEmailAndPassword(email, password).await()
-//            // delete collection from firebase
-//            store.collection(COLLECTION_PATH_PROFILES).whereEqualTo("email", email).get()
-//                .await().documents.forEach {
-//                    store.collection(COLLECTION_PATH_PROFILES).document(it.id).delete().await()
-//                }
-//            // delete profile image from firebase
-//            profileImageRepository.delete()
-//
-//            // delete user from firebase
-//            auth.currentUser?.delete()
-//        }
-//    }
+    private val store: FirebaseFirestore = setupFirestore()
+    private val storage: FirebaseStorage = setupFirebaseStorage()
+    private val auth: FirebaseAuth = setupFirebaseAuth()
+    private val profileRepository: ProfileRepository = ProfileRepository(store)
+    private val profileImageRepository: ProfileImageRepository = ProfileImageRepository(storage, auth)
 
-//    @SuppressLint("AssertionSideEffect")
-//    @Test
-//    fun testCheckForm() {
-//        val signUpViewModel = SignUpViewModel()
+    // Set input
 //
-//        // check that its not valid before adding it
-//        assert(!signUpViewModel.checkForm())
-//        assert(!signUpViewModel.isValidUsername(username))
-//        assert(signUpViewModel.profile.value.name == "")
+    private val userImage: Uri =
+        Uri.parse("android.resource://ch.epfl.sdp.cook4me/drawable/ic_launcher_foreground")
 //
-//        // create onSignUpFailure and onSignUpSuccess
-//        var isSignUpFailed = false
-//        var isSignUpSuccess = false
-//
-//        // check that that is not possible to sign up without an username
-//        signUpViewModel.onSubmit(
-//            onSignUpFailure = { isSignUpFailed = true },
-//            onSignUpSuccess = { isSignUpSuccess = true }
-//        )
-//
-//        // check that the function was called correctly
-//        assert(signUpViewModel.formError.value)
-//        assert(isSignUpFailed)
-//        assert(!isSignUpSuccess)
-//
-//        signUpViewModel.addUsername(username)
-//        signUpViewModel.addAllergies(allergies)
-//        signUpViewModel.addFavoriteDish(favoriteDish)
-//        signUpViewModel.addEmail(email)
-//        signUpViewModel.addBio(bio)
-//        signUpViewModel.addPassword(password)
-//        signUpViewModel.addProfileImage(userImage)
-//
-//        // check getters
-//        assert(signUpViewModel.profile.value.name == username)
-//        assert(signUpViewModel.profile.value.allergies == allergies)
-//        assert(signUpViewModel.profile.value.favoriteDish == favoriteDish)
-//        assert(signUpViewModel.profile.value.bio == bio)
-//        assert(signUpViewModel.profile.value.email == email)
-//        // assert(signUpViewModel.profileImage.toString() == userImage.toString()) //TODO does assert an error user something
-//
-//        // check that its valid after adding it all
-//        assert(signUpViewModel.checkForm())
-//        assert(signUpViewModel.formError.value)
-//
-//        isSignUpFailed = false
-//        isSignUpSuccess = false
-//
-//        signUpViewModel.onSubmit(
-//            onSignUpFailure = { isSignUpFailed = true },
-//            onSignUpSuccess = { isSignUpSuccess = true }
-//        )
-//
-//        // wait on signupSuccess
-//        composeTestRule.waitUntil(timeoutMillis = 5000) {
-//            isSignUpSuccess
-//        }
-//
-//        // check that the function was called correctly
-//        assert(!isSignUpFailed)
-//        assert(isSignUpSuccess)
-//
-//        // check that the user is created correctly
-//        val profileViewModel = ProfileViewModel()
-//
-//        // wait on profileViewModel
-//        composeTestRule.waitUntil(timeoutMillis = 5000) {
-//            !profileViewModel.isLoading.value
-//        }
-//
-//        // check that the user is created correctly
-//        assert(profileViewModel.profile.value.name == username)
-//        assert(profileViewModel.profile.value.favoriteDish == favoriteDish)
-//        assert(profileViewModel.profile.value.allergies == allergies)
-//        assert(profileViewModel.profile.value.bio == bio)
-//        assert(profileViewModel.profile.value.email == email)
-//    }
 
-//    @Test
-//    fun signupTwiceWithSameEmail() {
-//        val signUpViewModel = SignUpViewModel()
-//
-//        signUpViewModel.addUsername(username)
-//        signUpViewModel.addAllergies(allergies)
-//        signUpViewModel.addFavoriteDish(favoriteDish)
-//        signUpViewModel.addEmail(email)
-//        signUpViewModel.addBio(bio)
-//        signUpViewModel.addPassword(password)
-//
-//        // create onSignUpFailure and onSignUpSuccess
-//        var isSignUpFailed = false
-//        var isSignUpSuccess = false
-//
-//        // 1. signup
-//        signUpViewModel.onSubmit(
-//            onSignUpFailure = { isSignUpFailed = true },
-//            onSignUpSuccess = { isSignUpSuccess = true }
-//        )
-//
-//        // wait on signupSuccess
-//        composeTestRule.waitUntil(timeoutMillis = 5000) {
-//            isSignUpSuccess
-//        }
-//
-//        // reassign values
-//        isSignUpFailed = false
-//        isSignUpSuccess = false
-//
-//        // 2. signup
-//        signUpViewModel.onSubmit(
-//            onSignUpFailure = { isSignUpFailed = true },
-//            onSignUpSuccess = { isSignUpSuccess = true }
-//        )
-//
-//        // wait on signupSuccess
-//        composeTestRule.waitUntil(timeoutMillis = 5000) {
-//            isSignUpFailed
-//        }
-//    }
-// }
+    @After
+    fun cleanUp() {
+        runBlocking {
+            auth.signInWithEmailAndPassword(EMAIL, PASSWORD).await()
+            profileRepository.deleteAll()
+            profileImageRepository.deleteImageForCurrentUser()
+            auth.currentUser?.delete()?.await()
+        }
+    }
+
+    @SuppressLint("AssertionSideEffect")
+    @Test
+    fun testCheckForm() {
+        val signUpViewModel = SignUpViewModel()
+
+        // check that its not valid before adding it
+        assert(!signUpViewModel.checkForm())
+        assert(!signUpViewModel.isValidUsername(USERNAME))
+        assert(signUpViewModel.profile.value.name == "")
+
+        // create onSignUpFailure and onSignUpSuccess
+        var isSignUpFailed = false
+        var isSignUpSuccess = false
+
+        // check that that is not possible to sign up without an username
+        signUpViewModel.onSubmit(
+            onSignUpFailure = { isSignUpFailed = true },
+            onSignUpSuccess = { isSignUpSuccess = true }
+        )
+
+        // check that the function was called correctly
+        assert(signUpViewModel.formError.value)
+        assert(isSignUpFailed)
+        assert(!isSignUpSuccess)
+
+        signUpViewModel.addUsername(USERNAME)
+        signUpViewModel.addAllergies(ALLERGIES)
+        signUpViewModel.addFavoriteDish(FAVORITE_DISH)
+        signUpViewModel.addEmail(EMAIL)
+        signUpViewModel.addBio(BIO)
+        signUpViewModel.addPassword(PASSWORD)
+        signUpViewModel.addProfileImage(userImage)
+
+        // check getters
+        assert(signUpViewModel.profile.value.name == USERNAME)
+        assert(signUpViewModel.profile.value.allergies == ALLERGIES)
+        assert(signUpViewModel.profile.value.favoriteDish == FAVORITE_DISH)
+        assert(signUpViewModel.profile.value.bio == BIO)
+        assert(signUpViewModel.profile.value.email == EMAIL)
+
+        // check that its valid after adding it all
+        assert(signUpViewModel.checkForm())
+        assert(signUpViewModel.formError.value)
+
+        isSignUpFailed = false
+        isSignUpSuccess = false
+
+        signUpViewModel.onSubmit(
+            onSignUpFailure = { isSignUpFailed = true },
+            onSignUpSuccess = { isSignUpSuccess = true }
+        )
+
+        // wait on signupSuccess
+        composeTestRule.waitUntil(timeoutMillis = 5000) {
+            isSignUpSuccess
+        }
+
+        // check that the function was called correctly
+        assert(!isSignUpFailed)
+        assert(isSignUpSuccess)
+
+        // check that the user is created correctly
+        val profileViewModel = ProfileViewModel()
+
+        // wait on profileViewModel
+        composeTestRule.waitUntil(timeoutMillis = 5000) {
+            !profileViewModel.isLoading.value
+        }
+
+        // check that the user is created correctly
+        assert(profileViewModel.profile.value.name == USERNAME)
+        assert(profileViewModel.profile.value.favoriteDish == FAVORITE_DISH)
+        assert(profileViewModel.profile.value.allergies == ALLERGIES)
+        assert(profileViewModel.profile.value.bio == BIO)
+        assert(profileViewModel.profile.value.email == EMAIL)
+    }
+
+    @Test
+    fun signupTwiceWithSameEmail() {
+        val signUpViewModel = SignUpViewModel()
+
+        signUpViewModel.addUsername(USERNAME)
+        signUpViewModel.addAllergies(ALLERGIES)
+        signUpViewModel.addFavoriteDish(FAVORITE_DISH)
+        signUpViewModel.addEmail(EMAIL)
+        signUpViewModel.addBio(BIO)
+        signUpViewModel.addPassword(PASSWORD)
+
+        // create onSignUpFailure and onSignUpSuccess
+        var isSignUpFailed = false
+        var isSignUpSuccess = false
+
+        // 1. signup
+        signUpViewModel.onSubmit(
+            onSignUpFailure = { isSignUpFailed = true },
+            onSignUpSuccess = { isSignUpSuccess = true }
+        )
+
+        // wait on signupSuccess
+        composeTestRule.waitUntil(timeoutMillis = 5000) {
+            isSignUpSuccess
+        }
+
+        // reassign values
+        isSignUpFailed = false
+        isSignUpSuccess = false
+
+        // 2. signup
+        signUpViewModel.onSubmit(
+            onSignUpFailure = { isSignUpFailed = true },
+            onSignUpSuccess = { isSignUpSuccess = true }
+        )
+
+        // wait on signupSuccess
+        composeTestRule.waitUntil(timeoutMillis = 5000) {
+            isSignUpFailed
+        }
+    }
+ }
