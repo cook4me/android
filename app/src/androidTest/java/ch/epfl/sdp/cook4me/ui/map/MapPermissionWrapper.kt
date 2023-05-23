15b7e2d39f8dfed20d076fa9d73da85cdfaad910
dayan9265 <82887324+dayan9265@users.noreply.github.com>
2023-05-23 09:13:13 +0200

    ktlint + detekt

--- app/src/androidTest/java/ch/epfl/sdp/cook4me/ui/map/MapPermissionWrapper.kt
+++ app/src/androidTest/java/ch/epfl/sdp/cook4me/ui/map/MapPermissionWrapper.kt
@@ -1,19 +1,19 @@
 package ch.epfl.sdp.cook4me.ui.map
 
- import androidx.activity.ComponentActivity
- import androidx.compose.foundation.layout.fillMaxSize
- import androidx.compose.ui.Modifier
- import androidx.compose.ui.test.assertIsDisplayed
- import androidx.compose.ui.test.junit4.createAndroidComposeRule
- import androidx.compose.ui.test.onNodeWithText
- import androidx.test.ext.junit.runners.AndroidJUnit4
- import ch.epfl.sdp.cook4me.TestPermissionStatusProvider
- import org.junit.Rule
- import org.junit.Test
- import org.junit.runner.RunWith
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import ch.epfl.sdp.cook4me.TestPermissionStatusProvider
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
- @RunWith(AndroidJUnit4::class)
- class TestMapPermissionWrapper {
+@RunWith(AndroidJUnit4::class)
+class TestMapPermissionWrapper {
     @get:Rule
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
@@ -46,4 +46,4 @@ package ch.epfl.sdp.cook4me.ui.map
 
         composeTestRule.onNodeWithText("The location permission will grant a better experience in the app").assertIsDisplayed()
     }
- }
+}
