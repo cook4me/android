7c626894dbdb425427631c8aa9dbd730ea3525e2
Tachi-67 <haolongli_neko@outlook.com>
2023-05-24 15:07:33 +0200

    refactor loading

--- app/src/main/java/ch/epfl/sdp/cook4me/ui/common/LoadingScreen.kt
+++ app/src/main/java/ch/epfl/sdp/cook4me/ui/common/LoadingScreen.kt
@@ -1,47 +1,52 @@
 package ch.epfl.sdp.cook4me.ui.common
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.animation.core.*
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import ch.epfl.sdp.cook4me.R
 import kotlinx.coroutines.delay
 
-const val DELAY_TIME = 100L
-const val MOD_NUMBER = 3
-const val ADD_NUMBER = 1
-
-// A loading screen that is displayed when the user is waiting for the chat to load
-// It shows a changing text with different number of dots
+val visibility = listOf<Float>(0.4f, 0.6f, 0.8f, 1.0f)
+const val DELAY_TIME = 80L
+const val MOD_NUMBER = 4
 @Composable
 fun LoadingScreen() {
-    val loadingDots = remember { mutableStateOf(1) }
-
+    val imageResource = R.drawable.loadingpic
+    val loadingIndex = remember { mutableStateOf(0) }
     LaunchedEffect(Unit) {
         while (true) {
             delay(DELAY_TIME)
-            loadingDots.value = loadingDots.value % MOD_NUMBER + ADD_NUMBER
+            loadingIndex.value = (loadingIndex.value + 1) % MOD_NUMBER
         }
     }
 
-    Column(
+    Box(
+        contentAlignment = Alignment.Center,
         modifier = Modifier
             .fillMaxSize()
             .testTag(stringResource(R.string.Loading_Screen_Tag)),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-
     ) {
-        Text(
-            text = "${stringResource(R.string.loading_text)}${".".repeat(loadingDots.value)}"
+        Image(
+            painter = painterResource(imageResource),
+            contentDescription = "Loading icon",
+            alpha = visibility[loadingIndex.value],
+            modifier = Modifier.size(100.dp) // Change the size as needed
         )
     }
 }
+
