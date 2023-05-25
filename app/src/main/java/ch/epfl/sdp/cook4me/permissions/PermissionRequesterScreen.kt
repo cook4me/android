0f9a89f4af7ecae7ec5ceb1f1bda95c8174167ad
pauromeu <romeu.pau@gmail.com>
2023-05-25 18:51:38 +0200

    remove unused import

--- app/src/main/java/ch/epfl/sdp/cook4me/permissions/PermissionRequesterScreen.kt
+++ app/src/main/java/ch/epfl/sdp/cook4me/permissions/PermissionRequesterScreen.kt
@@ -19,7 +19,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ch.epfl.sdp.cook4me.R
 
