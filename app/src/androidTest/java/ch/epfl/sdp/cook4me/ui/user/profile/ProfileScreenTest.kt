6c02d02f127b0e3d0b41fbde8d0b38927e1c5ce0
bu-da <daniel.bucher@healthinal.com>
2023-05-26 01:03:15 +0200

    do proper comment out

--- app/src/androidTest/java/ch/epfl/sdp/cook4me/ui/user/profile/ProfileScreenTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/cook4me/ui/user/profile/ProfileScreenTest.kt
@@ -1,74 +1,71 @@
 package ch.epfl.sdp.cook4me.ui.user.profile
+//
+//import android.net.Uri
+//import androidx.activity.ComponentActivity
+//import androidx.compose.ui.test.junit4.createAndroidComposeRule
+//import androidx.test.ext.junit.runners.AndroidJUnit4
+//import ch.epfl.sdp.cook4me.R
+//import ch.epfl.sdp.cook4me.application.AccountService
+//import ch.epfl.sdp.cook4me.persistence.model.Profile
+//import ch.epfl.sdp.cook4me.persistence.repository.ProfileImageRepository
+//import ch.epfl.sdp.cook4me.persistence.repository.ProfileRepository
+//import ch.epfl.sdp.cook4me.setupFirebaseAuth
+//import ch.epfl.sdp.cook4me.setupFirebaseStorage
+//import ch.epfl.sdp.cook4me.setupFirestore
+//import com.google.firebase.auth.FirebaseAuth
+//import com.google.firebase.firestore.FirebaseFirestore
+//import com.google.firebase.storage.FirebaseStorage
+//import kotlinx.coroutines.runBlocking
+//import kotlinx.coroutines.tasks.await
+//import org.junit.After
+//import org.junit.Before
+//import org.junit.Rule
+//import org.junit.runner.RunWith
+//
+//private const val USERNAME = "donald.duck@epfl.ch"
+//private const val PASSWORD = "123456"
+//
+//@RunWith(AndroidJUnit4::class)
+//class ProfileScreenTest {
+//    @get:Rule
+//    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+//
+//    private val auth: FirebaseAuth = setupFirebaseAuth()
+//    private val store: FirebaseFirestore = setupFirestore()
+//    private val storage: FirebaseStorage = setupFirebaseStorage()
+//    private val account: AccountService = AccountService(auth)
+//    private val repository: ProfileRepository = ProfileRepository(store)
+//    private val profileImageRepository: ProfileImageRepository = ProfileImageRepository(storage, auth)
+//    private val profileImage = Uri.parse("android.resource://ch.epfl.sdp.cook4me/drawable/" + R.drawable.ic_user)
+//    private val user = Profile(
+//        email = USERNAME,
+//        allergies = "Hazelnut",
+//        bio = "I am a duck",
+//        favoriteDish = "Spaghetti",
+//    )
+//
+//    @Before
+//    fun setUp() {
+//        runBlocking {
+//            auth.createUserWithEmailAndPassword(USERNAME, PASSWORD).await()
+//            auth.signInWithEmailAndPassword(USERNAME, PASSWORD).await()
+//            repository.add(user)
+//            profileImageRepository.add(profileImage)
+//        }
+//    }
+//
+//    @After
+//    fun cleanUp() {
+//        runBlocking {
+//            // delete the user from the database
+//            repository.deleteAll()
+//            profileImageRepository.deleteImageForCurrentUser()
+//            auth.signInWithEmailAndPassword(USERNAME, PASSWORD).await()
+//            auth.currentUser?.delete()?.await()
+//        }
+//    }
 
-import android.net.Uri
-import androidx.activity.ComponentActivity
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onAllNodesWithText
-import androidx.compose.ui.test.onNodeWithText
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import ch.epfl.sdp.cook4me.R
-import ch.epfl.sdp.cook4me.application.AccountService
-import ch.epfl.sdp.cook4me.persistence.model.Profile
-import ch.epfl.sdp.cook4me.persistence.repository.ProfileImageRepository
-import ch.epfl.sdp.cook4me.persistence.repository.ProfileRepository
-import ch.epfl.sdp.cook4me.setupFirebaseAuth
-import ch.epfl.sdp.cook4me.setupFirebaseStorage
-import ch.epfl.sdp.cook4me.setupFirestore
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.storage.FirebaseStorage
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.tasks.await
-import org.junit.After
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
-import org.junit.runner.RunWith
-
-private const val USERNAME = "donald.duck@epfl.ch"
-private const val PASSWORD = "123456"
-
-@RunWith(AndroidJUnit4::class)
-class ProfileScreenTest {
-    @get:Rule
-    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
-
-    private val auth: FirebaseAuth = setupFirebaseAuth()
-    private val store: FirebaseFirestore = setupFirestore()
-    private val storage: FirebaseStorage = setupFirebaseStorage()
-    private val account: AccountService = AccountService(auth)
-    private val repository: ProfileRepository = ProfileRepository(store)
-    private val profileImageRepository: ProfileImageRepository = ProfileImageRepository(storage, auth)
-    private val profileImage = Uri.parse("android.resource://ch.epfl.sdp.cook4me/drawable/" + R.drawable.ic_user)
-    private val user = Profile(
-        email = USERNAME,
-        allergies = "Hazelnut",
-        bio = "I am a duck",
-        favoriteDish = "Spaghetti",
-    )
-
-    @Before
-    fun setUp() {
-        runBlocking {
-            auth.createUserWithEmailAndPassword(USERNAME, PASSWORD).await()
-            auth.signInWithEmailAndPassword(USERNAME, PASSWORD).await()
-            repository.add(user)
-            profileImageRepository.add(profileImage)
-        }
-    }
-
-    @After
-    fun cleanUp() {
-        runBlocking {
-            // delete the user from the database
-            repository.deleteAll()
-            profileImageRepository.deleteImageForCurrentUser()
-            auth.signInWithEmailAndPassword(USERNAME, PASSWORD).await()
-            auth.currentUser?.delete()?.await()
-        }
-    }
-
-    //TODO: Fix me
+    // TODO: Fix me
 //    @Test
 //    fun profileInfoIsDisplayed() {
 //        composeTestRule.setContent {
